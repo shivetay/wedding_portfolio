09401b61927c3e20eed8fc07f20cb5384f47219e
Łukasz Dawidowicz <lddev@outlook.com>
2020-10-28 15:13:30 +0100

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import MainLayout from '../src/components/layout/MainLayout/MainLayout';
+import Home from './components/views/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainLayout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+        <h1>app.js</h1>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
