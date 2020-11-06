3adab6674e50c535ea36fdaae0ef0b543a5c1073
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-06 15:20:24 +0100

    add: basic styling

--- src/components/views/About/About.js
+++ src/components/views/About/About.js
@@ -8,21 +8,24 @@ const About = () => {
   return (
     <section className='About'>
       <div className='About__Container'>
-        <div className='About__Photo'>
-          <img
-            className='About__Photo-img'
-            src={about}
-            alt='photo Dorota Dawidowicz'
-          />
-        </div>
-        <div className='About__Text'>
-          <h2>Litwo! Ojczyzno moja!</h2>
-          <p>
-            Pierwsza z krzykiem podróżnego ściskał i z jego pierś powabną suknię
-            materyjalną, różową, jedwabną gors wycięty, kołnierzyk z jutrzenką
-            napotka się pomieszany, zły i w uczciwości, w skroń ucałowawszy,
-            uprzejmie pozdrowił. A na wychowanie poznano stołeczne.
-          </p>
+        <div className='About__Content'>
+          <div className='About__Photo'>
+            <img
+              className='About__Photo-img'
+              src={about}
+              alt='photo Dorota Dawidowicz'
+            />
+          </div>
+          <div className='About__Text'>
+            <h2>Litwo! Ojczyzno moja!</h2>
+            <p>
+              Pierwsza z krzykiem podróżnego ściskał i z jego pierś powabną
+              suknię materyjalną, różową, jedwabną gors wycięty, kołnierzyk z
+              jutrzenką napotka się pomieszany, zły i w uczciwości, w skroń
+              ucałowawszy, uprzejmie pozdrowił. A na wychowanie poznano
+              stołeczne.
+            </p>
+          </div>
         </div>
       </div>
     </section>
