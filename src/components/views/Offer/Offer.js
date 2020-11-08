0e1d6737eb86d1239efc426c2a86f1ae3b563df3
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-08 17:30:46 +0100

    add: elemnt styling

--- src/components/views/Offer/Offer.js
+++ src/components/views/Offer/Offer.js
@@ -10,30 +10,33 @@ const Offer = () => {
           <img
             className='Offer-img'
             src='https://images.unsplash.com/photo-1520182062070-2e037a6fbbba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+            alt='offer_photo'
           />
-          <div>
+          <div className='Offer__Content-text'>
             <p>Lorem</p>
-            <button>Więcej</button>
+            <button className='btn'>Więcej</button>
           </div>
         </div>
         <div className='Offer__Content'>
-          <div>
+          <div className='Offer__Content-text'>
             <p>Lorem</p>
-            <button>Więcej</button>
+            <button className='btn'>Więcej</button>
           </div>
           <img
             className='Offer-img'
             src='https://images.unsplash.com/photo-1537291167-7f1592be546c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80'
+            alt='offer_photo'
           />
         </div>
         <div className='Offer__Content'>
           <img
             className='Offer-img'
             src='https://images.unsplash.com/photo-1524777313293-86d2ab467344?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+            alt='offer_photo'
           />
-          <div>
+          <div className='Offer__Content-text'>
             <p>Lorem</p>
-            <button>Więcej</button>
+            <button className='btn'>Więcej</button>
           </div>
         </div>
       </div>
