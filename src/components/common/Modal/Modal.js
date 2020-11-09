62d0858b9a3ca2b4e2ce9d11216dd37bb077ef7a
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-09 11:33:00 +0100

    work: on modal

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -1,37 +1,67 @@
-import React, { Fragment } from 'react';
-import ReactDOM from 'react-dom';
+// import React, { Fragment } from 'react';
+// import ReactDOM from 'react-dom';
+// import PropTypes from 'prop-types';
+
+// import './Modal.scss';
+
+// import Backdrop from './Backdrop';
+
+// const ModalOverlay = (props) => {
+//   const content = (
+//     <div className={`Modal ${props.className}`}>
+//       <header className={`Modal-header ${props.headerClass}`}>
+//         <h2>{props.header}</h2>
+//       </header>
+//       <div className={`Modal-content ${props.contentClass}`}>
+//         {props.children}
+//       </div>
+//     </div>
+//   );
+//   return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
+// };
+
+// const Modal = (props) => {
+//   return (
+//     <Fragment>
+//       {props.show && <Backdrop onClick={props.onCancel} />}
+//       <ModalOverlay {...props} />
+//     </Fragment>
+//   );
+// };
+
+// Modal.propTypes = {
+//   show: PropTypes.any,
+//   onCancel: PropTypes.any,
+// };
+
+// export default Modal;
+
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Modal.scss';
 
-import Backdrop from './Backdrop';
-
-const ModalOverlay = (props) => {
-  const content = (
-    <div className={`Modal ${props.className}`}>
-      <header className={`Modal-header ${props.headerClass}`}>
-        <h2>{props.header}</h2>
-      </header>
-      <div className={`Modal-content ${props.contentClass}`}>
-        {props.children}
-      </div>
-    </div>
-  );
-  return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
-};
-
-const Modal = (props) => {
+const Modal = ({ title, text, show, close }) => {
   return (
-    <Fragment>
-      {props.show && <Backdrop onClick={props.onCancel} />}
-      <ModalOverlay {...props} />
-    </Fragment>
+    <div
+      className='Modal__Container'
+      style={{
+        display: show ? 'inline' : 'none',
+      }}>
+      <div className='Modal__Content'>
+        <h2>{title}</h2>
+        <p>{text}</p>
+      </div>
+      <button onClick={close}>Zamknij</button>
+    </div>
   );
 };
 
 Modal.propTypes = {
-  show: PropTypes.any,
-  onCancel: PropTypes.any,
+  title: PropTypes.string,
+  text: PropTypes.string,
+  show: PropTypes.bool,
+  close: PropTypes.func,
 };
 
 export default Modal;
--- src/components/views/Offer/Offer.js
+++ src/components/views/Offer/Offer.js
@@ -1,53 +1,76 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Offer.scss';
 
-const Offer = () => {
-  return (
-    <section className='Offer'>
-      <div className='Offer__Container'>
-        <div className='Offer__Content'>
-          <img
-            className='Offer-img'
-            src='https://images.unsplash.com/photo-1520182062070-2e037a6fbbba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
-            alt='offer_photo'
-          />
-          <div className='Offer__Content-text'>
-            <h3>Tadeusz</h3>
-            <p>
-              Wreszcie po całym domu dawne obyczaje swe rodzinne duszę
-              utęsknioną
-            </p>
-            <button className='btn btn-offer'>Więcej</button>
+import Modal from '../../common/Modal/Modal';
+
+class Offer extends Component {
+  state = {
+    showModal: false,
+  };
+
+  openModal = () => {
+    this.setState({ showModal: true });
+    console.log('modal');
+  };
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+  render() {
+    const { showModal } = this.state;
+    return (
+      <section className='Offer'>
+        <div className='Offer__Container'>
+          <div className='Offer__Content'>
+            <img
+              className='Offer-img'
+              src='https://images.unsplash.com/photo-1520182062070-2e037a6fbbba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+              alt='offer_photo'
+            />
+            <div className='Offer__Content-text'>
+              <h3>Tadeusz</h3>
+              <p>
+                Wreszcie po całym domu dawne obyczaje swe rodzinne duszę
+                utęsknioną
+              </p>
+              <button
+                onClick={() => this.openModal()}
+                className='btn btn-offer'>
+                Więcej
+              </button>
+            </div>
+            <Modal show={showModal} close={() => this.closeModal()} />
+          </div>
+          <div className='Offer__Content'>
+            <Modal />
+            <div className='Offer__Content-text'>
+              <h3>Krzyczano</h3>
+              <p> Bo nie był ruchawy od płaczącej matki pod lasem zwaliska.</p>
+              <button className='btn btn-offer'>Więcej</button>
+            </div>
+            <img
+              className='Offer-img'
+              src='https://images.unsplash.com/photo-1537291167-7f1592be546c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80'
+              alt='offer_photo'
+            />
+          </div>
+          <div className='Offer__Content'>
+            <img
+              className='Offer-img'
+              src='https://images.unsplash.com/photo-1524777313293-86d2ab467344?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+              alt='offer_photo'
+            />
+            <div className='Offer__Content-text'>
+              <h3>Litwę muram</h3>
+              <p>Dano trzecią potrawę.</p>
+              <button className='btn btn-offer'>Więcej</button>
+            </div>
+            <Modal />
           </div>
         </div>
-        <div className='Offer__Content'>
-          <div className='Offer__Content-text'>
-            <h3>Krzyczano</h3>
-            <p> Bo nie był ruchawy od płaczącej matki pod lasem zwaliska.</p>
-            <button className='btn btn-offer'>Więcej</button>
-          </div>
-          <img
-            className='Offer-img'
-            src='https://images.unsplash.com/photo-1537291167-7f1592be546c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80'
-            alt='offer_photo'
-          />
-        </div>
-        <div className='Offer__Content'>
-          <img
-            className='Offer-img'
-            src='https://images.unsplash.com/photo-1524777313293-86d2ab467344?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
-            alt='offer_photo'
-          />
-          <div className='Offer__Content-text'>
-            <h3>Litwę muram</h3>
-            <p>Dano trzecią potrawę.</p>
-            <button className='btn btn-offer'>Więcej</button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default Offer;
