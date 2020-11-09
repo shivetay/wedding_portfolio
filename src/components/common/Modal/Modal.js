b39218baeb66c1fbcbcbe6fb99d1c072fdf6a62f
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-09 13:33:27 +0100

    add: modal styling overlay, change itmes render

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -1,47 +1,9 @@
-// import React, { Fragment } from 'react';
-// import ReactDOM from 'react-dom';
-// import PropTypes from 'prop-types';
-
-// import './Modal.scss';
-
-// import Backdrop from './Backdrop';
-
-// const ModalOverlay = (props) => {
-//   const content = (
-//     <div className={`Modal ${props.className}`}>
-//       <header className={`Modal-header ${props.headerClass}`}>
-//         <h2>{props.header}</h2>
-//       </header>
-//       <div className={`Modal-content ${props.contentClass}`}>
-//         {props.children}
-//       </div>
-//     </div>
-//   );
-//   return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
-// };
-
-// const Modal = (props) => {
-//   return (
-//     <Fragment>
-//       {props.show && <Backdrop onClick={props.onCancel} />}
-//       <ModalOverlay {...props} />
-//     </Fragment>
-//   );
-// };
-
-// Modal.propTypes = {
-//   show: PropTypes.any,
-//   onCancel: PropTypes.any,
-// };
-
-// export default Modal;
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Modal.scss';
 
-const Modal = ({ title, text, show, close }) => {
+const Modal = ({ show, close, modalHeader, modalText }) => {
   return (
     <div
       className='Modal__Container'
@@ -49,17 +11,17 @@ const Modal = ({ title, text, show, close }) => {
         display: show ? 'inline' : 'none',
       }}>
       <div className='Modal__Content'>
-        <h2>{title}</h2>
-        <p>{text}</p>
+        <h2>{modalHeader}</h2>
+        <p>{modalText}</p>
+        <button onClick={close}>Zamknij</button>
       </div>
-      <button onClick={close}>Zamknij</button>
     </div>
   );
 };
 
 Modal.propTypes = {
-  title: PropTypes.string,
-  text: PropTypes.string,
+  modalHeader: PropTypes.string,
+  modalText: PropTypes.string,
   show: PropTypes.bool,
   close: PropTypes.func,
 };
