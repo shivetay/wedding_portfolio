0e58c936f3b5d0fa4b1fea1a352e931bc108fcf6
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-10 13:18:23 +0100

    add: contact init

---
+++ src/components/views/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../../layout/Footer/Footer';
+
+const Contact = () => {
+  return (
+    <div>
+      <p>contact page</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default Contact;
