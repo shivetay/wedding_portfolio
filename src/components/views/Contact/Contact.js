dae8edf0546dcad76005718fe0cada88d86781ac
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-10 17:12:51 +0100

    add: styling

--- src/components/views/Contact/Contact.js
+++ src/components/views/Contact/Contact.js
@@ -1,12 +1,50 @@
 import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+import logo from '../../../Images/logo_bez tła[12398].png';
+
+import './Contact.scss';
+
 import Footer from '../../layout/Footer/Footer';
 
 const Contact = () => {
   return (
-    <div>
-      <p>contact page</p>
+    <section className='Contact'>
+      <div className='Contact__Container'>
+        <div className='Contact__Header'>
+          <img className='Contact-img' src={logo} alt='logo_Bohho' />
+        </div>
+        <div className='Contact__Content'>
+          <h3>Bohho - organizacja ślubów i imprez</h3>
+          <p>Wrocław</p>
+          <p>NIP: 8992578165</p>
+          <p>
+            <FontAwesomeIcon icon={faPhone} /> Tel.: 887-623-220
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faEnvelope} /> bohho[at]bohho.eu
+          </p>
+          <div className='Contact__Content-icons'>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.facebook.com/bohho.sluby.imprezy'>
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.instagram.com/bohho_sluby_imprezy/'>
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </div>
+        </div>
+      </div>
       <Footer />
-    </div>
+    </section>
   );
 };
 
