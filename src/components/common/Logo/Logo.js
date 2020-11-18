92e09d771fefb90a6f2b5f8a64a2495de179312f
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-18 21:47:32 +0100

    add: icons to navbar, fix styling

--- src/components/common/Logo/Logo.js
+++ src/components/common/Logo/Logo.js
@@ -7,9 +7,11 @@ import logo from '../../../Images/logo2_bez_tla.png';
 
 const Logo = () => {
   return (
-    <Link className='Logo' to='/'>
-      <img className='Logo__Img' src={logo} alt='bohho_logo' />
-    </Link>
+    <div className='Logo'>
+      <Link to='/'>
+        <img className='Logo__Img' src={logo} alt='bohho_logo' />
+      </Link>
+    </div>
   );
 };
 
