169acf69205a176f5818af0eafba45e2a621e1b4
Łukasz Dawidowicz <lddev@outlook.com>
2020-11-23 11:32:36 +0100

    add: decorationa elemnt for offer page

--- src/components/views/Offer/Offer.js
+++ src/components/views/Offer/Offer.js
@@ -4,26 +4,28 @@ import './Offer.scss';
 
 import Modal from '../../common/Modal/Modal';
 
+import button from '../../../Images/ciapka5.png';
+
 class Offer extends Component {
   state = {
     offerItem: [
       {
         id: 1,
-        offerHeader: 'ŚLUBY I WESELA',
+        offerHeader: 'Śluby i Wesela',
         modalText:
           'Mówią, że to najważniejszy dzień w życiu. I ja się z tym zgadzam. A jeśli chcecie, żeby ten Wasz dzień był do tego niezapomniany i idealny, to z przyjemnością się tym zajmę. •	Kompleksowa organizacja ślubu i wesela Przygotowanie „od A do Z”. To ja szukam i umawiam wszystkich podwykonawców, zajmuję się całym przebiegiem przygotowań, by ostatecznie koordynować Wasz dzień. •	Częściowa organizacja ślubu i wesela Przygotowanie tych punktów w ślubnym harmonogramie, którymi Wy nie chcecie, lub nie możecie się zająć. •	Koordynacja ślubu i wesela Jeśli sami wszystko przygotowaliście, ale chcecie, żeby ktoś czuwał nad Waszym dniem, by wszystko było tak, jak sobie wymarzyliście, to to opcja jest właśnie dla Was`',
         modalHeader: 'ŚLUBY I WESELA',
       },
       {
         id: 2,
-        offerHeader: 'PRZYJĘCIA I IMPREZY RODZINNE',
+        offerHeader: 'Przyjęcia i Imprezy Rodzinne',
         modalText:
           'Chętnie zorganizuję dla Was niezapomniane przyjęcia i uroczystości rodzinne. Kameralnie, czy z pompą – to już zależy od Was•	Zaręczyny•	Chrzciny•	Babyshower •	Urodziny•	Okolicznościowe przyjęcia rodzinne•	Każde inne, o jakim marzycie',
         modalHeader: 'PRZYJĘCIA I IMPREZY RODZINNE',
       },
       {
         id: 3,
-        offerHeader: 'SPOTKANIA I IMPREZY FIRMOWE',
+        offerHeader: 'Spotkania i Imprezy Firmowe',
         modalText:
           'Te duże i te małe – w końcu każdy chce czegoś innego. Dostosuję się do Waszych preferencji i przygotuję imprezę szytą na miarę•	Imprezy integracyjne stacjonarne•	Imprezy integracyjne wyjazdowe•	Spotkania biznesowe•	Imprezy firmowe okolicznościowe (świąteczne, karnawałowe, mikołajki, ogniska itp.)•	Eventy branżowe•	Inne ',
         modalHeader: 'SPOTKANIA I IMPREZY FIRMOWE',
@@ -49,6 +51,11 @@ class Offer extends Component {
                     className='btn btn-offer'>
                     Więcej
                   </button>
+                  <img
+                    className='Offer__Content-deco'
+                    src={button}
+                    alt='decom_element'
+                  />
                   <Modal
                     show={showModal === item.id}
                     classModal={'btn-offer'}
